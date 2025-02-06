816bec1954e9d571bca40de5092fea728decfb28
Mohamed <medgasmidev@gmail.com>
2025-02-06 14:24:54 +0100

    add the interfaces to manage users

---
+++ src/Form/UserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'firstName',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'lastName',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'email',
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'phone',
+            ])
+            ->add('birthday', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+// tests/Controller/UserControllerTest.php
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testUserCreation()
+    {
+        $user = new User();
+        $user->setFirstName('John');
+        $user->setLastName('Doe');
+        $user->setEmail('john.doe@example.com');
+        $user->setPhone('0758062008');
+
+        $this->assertEquals('John', $user->getFirstName());
+        $this->assertEquals('Doe', $user->getLastName());
+        $this->assertEquals('0758062008', $user->getPhone());
+        $this->assertEquals('john.doe@example.com', $user->getEmail());
+    }
+}
